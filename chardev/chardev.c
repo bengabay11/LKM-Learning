e0e30be0ab06b42062f19607c77cb8c3bf47cb38
Kali <ben.gabay38@gmail.com>
2022-07-29 11:10:09 -0400

    change functions order and change device name

--- chardev/chardev.c
+++ chardev/chardev.c
@@ -7,7 +7,7 @@
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 
-#define DEVICE_NAME "test"
+#define DEVICE_NAME "mychardev"
 #define BUF_LEN 80
 
 MODULE_LICENSE("MIT");
@@ -19,49 +19,48 @@ static int Device_Open = 0;
 static char msg[BUF_LEN];
 static char *msg_Ptr;
 
-static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
-{
-	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
-	return -EINVAL;
-}
-
-static ssize_t device_read(struct file* flip, char* buffer, size_t length, loff_t* offset)
-{
-	int bytes_read = 0;
-	if (*msg_Ptr == 0)
-		return 0;
-	while (length && *msg_Ptr)
-	{
-		put_user(*(msg_Ptr++), buffer++);
-		length--;
-		bytes_read++;
-	}
-	return bytes_read;
-}
-
 static int device_open(struct inode *inode, struct file *file)
 {
-	static int counter = 0;
-	if (Device_Open)
-	{
-		return -EBUSY;
-	}
-	Device_Open++;
-	sprintf(msg, "I already told you %d times Hello world!\n", counter++);
-	msg_Ptr = msg;
-	try_module_get(THIS_MODULE);
-	return 0;
+    static int counter = 0;
+    if (Device_Open)
+    {
+        return -EBUSY;
+    }
+    Device_Open++;
+    sprintf(msg, "I already told you %d times Hello world!\n", counter++);
+    msg_Ptr = msg;
+    try_module_get(THIS_MODULE);
+    return 0;
 }
 
 static int device_release(struct inode *inode, struct file *file)
 {
-	Device_Open--;
-	module_put(THIS_MODULE);
-	return 0;
+    Device_Open--;
+    module_put(THIS_MODULE);
+    return 0;
 }
 
-static struct file_operations fops = {
-	.owner = THIS_MODULE,
+static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+{
+    printk(KERN_ALERT "Write operation isn't supported.\n");
+    return -EINVAL;
+}
+
+static ssize_t device_read(struct file* flip, char* buffer, size_t length, loff_t* offset)
+{
+    int bytes_read = 0;
+    if (*msg_Ptr == 0)
+        return 0;
+    while (length && *msg_Ptr)
+    {
+        put_user(*(msg_Ptr++), buffer++);
+        length--;
+        bytes_read++;
+    }
+    return bytes_read;
+}
+
+struct file_operations fops = {
 	.read = device_read,
 	.write = device_write,
 	.open = device_open,
@@ -75,9 +74,9 @@ static int __init start_module(void)
 	{
 		printk(KERN_ALERT "Registering char device failed with %d\n", Device_Major);
 	}
-	printk(KERN_INFO "I was assigned major number %d. To talk to\n", Device_Major);
-	printk(KERN_INFO "the driver, create a dev file with 'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Device_Major);
-	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to the device file\n");
+	printk(KERN_INFO "The device %s assigned to major number %d\n", DEVICE_NAME, Device_Major);
+	printk(KERN_INFO "to talk to the driver, create a dev file with 'mknod /dev/%s c %d 0'\n", DEVICE_NAME, Device_Major);
+	printk(KERN_INFO "Try to cat and echo to the device file\n");
 	return 0;
 }
 
